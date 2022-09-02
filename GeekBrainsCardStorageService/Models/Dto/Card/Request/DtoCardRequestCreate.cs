c1dffcaa3b488ccc056d3b5e9e25b3eb266a0b41
Лесиков Сергей Игоревич <lesikov@net.crocusgroup.ru>
2022-09-02 17:50:41 +0300

    update

--- GeekBrainsCardStorageService/Models/Dto/Card/Request/DtoCardRequestCreate.cs
+++ GeekBrainsCardStorageService/Models/Dto/Card/Request/DtoCardRequestCreate.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GeekBrainsCardStorageService.Models.Dto.Card.Request
 {
     public class DtoCardRequestCreate
     {
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(55)]
         public string CardNumber { get; set; }
 
+        [Required]
+        [StringLength(5)]
         public string CVV2 { get; set; }
 
+        [Required]
         public DateTime ExpDate { get; set; }
 
+        [Required]
+        [Range(1, Int32.MaxValue)]
         public int ClientId { get; set; }
     }
 }
--- GeekBrainsCardStorageService/Models/Dto/Client/Request/DtoClientRequestCreate.cs
+++ GeekBrainsCardStorageService/Models/Dto/Client/Request/DtoClientRequestCreate.cs
@@ -1,11 +1,19 @@
-﻿namespace GeekBrainsCardStorageService.Models.Dto.Client.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeekBrainsCardStorageService.Models.Dto.Client.Request
 {
     public class DtoClientRequestCreate
     {
+        [Required]
+        [StringLength(55)]
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(55)]
         public string Surname { get; set; }
 
+        [Required]
+        [StringLength(55)]
         public string Patronymic { get; set; }
     }
 }

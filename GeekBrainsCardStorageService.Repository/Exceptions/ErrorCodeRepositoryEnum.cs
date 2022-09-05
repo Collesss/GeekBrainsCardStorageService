90654db3b5142a517f9daff92c2ad44a06d22c19
Лесиков Сергей Игоревич <lesikov@net.crocusgroup.ru>
2022-09-05 16:49:31 +0300

    update

---
+++ GeekBrainsCardStorageService.Repository/Exceptions/ErrorCodeRepositoryEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace GeekBrainsCardStorageService.Repository.Exceptions
+{
+    public enum ErrorCodeRepositoryEnum : int
+    {
+        None = 0x0000_0000__0000_0000__0000_0000__0000_0000
+    }
+}
